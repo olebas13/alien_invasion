2590c2c4f4dacec6f9888da3e4d06145da70c96c
olebas13 <olebas13@gmail.com>
2020-04-23 16:54:02 +0300

    init commit

---
+++ game_functions.py
@@ -0,0 +1,21 @@
+import sys
+
+import pygame
+
+
+def check_events(ship):
+    """Обрабатывает нажатия клавиш и события мыши."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx += 1
+
+def update_screen(ai_settings, screen, ship):
+    """Обновляет изображения на экране и отображает новый экран"""
+    # При каждом проходе цикла перерисовывается экран
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # Отображение последнего прорисованного экрана
+    pygame.display.flip()
