b01e9ddd7ac6c86f387d7b55dc82aacca584b218
olebas13 <olebas13@gmail.com>
2020-04-23 19:45:29 +0300

    refactor bullet functions

--- game_functions.py
+++ game_functions.py
@@ -11,8 +11,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
 
 def check_keyup_events(event, ship):
@@ -45,3 +44,18 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # Отображение последнего прорисованного экрана
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """Обновляет позиции пуль и уничтожает старые пули"""
+    # Обновление позиций пуль
+    bullets.update()
+    # Удаление пуль, вышедших за край экрана
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Выпускает пулю, если максимум еще не достигнут"""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
